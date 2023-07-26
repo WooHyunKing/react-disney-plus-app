db929ead8be442903de1818216c1bb3d37c645e5
신 우현 <woo_hyun_1@naver.com>
2023-07-26 20:58:43 +0900

    [Chap 1-7] Create Row component

--- src/components/Row.tsx
+++ src/components/Row.tsx
@@ -1,7 +1,54 @@
-import React from "react";
+import React, { useCallback, useEffect, useState } from "react";
+import instance from "../api/axios";
+import { IMovie } from "../interfaces/movieInterface";
+import "./Row.css";
 
-const Row = () => {
-  return <div></div>;
+const Row = ({
+  title,
+  id,
+  fetchUrl,
+}: {
+  title: string;
+  id: string;
+  fetchUrl: string;
+}) => {
+  const [movies, setMovies] = useState<IMovie[]>([]);
+
+  const fetchMovies = useCallback(async () => {
+    const response = await instance.get(fetchUrl);
+    console.log(response.data.results);
+    setMovies(response.data.results);
+    // setMovies(data);
+  }, [fetchUrl]);
+
+  useEffect(() => {
+    fetchMovies();
+  }, [fetchMovies]);
+
+  return (
+    <div>
+      <h2>{title}</h2>
+      <div className="slider">
+        <div className="slider__arrow-left">
+          <span className="arrow">{"<"}</span>
+        </div>
+        <div className="row__posters">
+          {movies.map((movie) => (
+            <img
+              key={movie.id}
+              className="row__poster"
+              src={`https://image.tmdb.org/t/p/original/${movie.backdrop_path}`}
+              alt={movie.name}
+              onClick={() => {}}
+            />
+          ))}
+        </div>
+        <div className="slider__arrow-right">
+          <span className="arrow">{">"}</span>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Row;
