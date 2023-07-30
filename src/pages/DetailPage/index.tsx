f4accac48e463ddb29ae5591042213145b340b5f
신 우현 <woo_hyun_1@naver.com>
2023-07-30 16:44:16 +0900

    [Chap 2-5] Create Detail page

--- src/pages/DetailPage/index.tsx
+++ src/pages/DetailPage/index.tsx
@@ -1,7 +1,39 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { IMovie } from "../../interfaces/movieInterface";
+import instance from "../../api/axios";
+import { styled } from "styled-components";
 
 const DetailPage = () => {
-  return <div></div>;
+  let { movieId } = useParams();
+  const [movie, setMovie] = useState<IMovie>({});
+
+  console.log(movie);
+
+  const fetchData = async () => {
+    const response = await instance.get(
+      `https://api.themoviedb.org/3/movie/${movieId}`
+    );
+    setMovie(response.data);
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, [movieId]);
+
+  if (!movie) {
+    return null;
+  }
+
+  return (
+    <section>
+      <img
+        className="modal__poster-img"
+        alt="modal__poster-img"
+        src={`https://image.tmdb.org/t/p/original/${movie.backdrop_path}`}
+      />
+    </section>
+  );
 };
 
 export default DetailPage;
